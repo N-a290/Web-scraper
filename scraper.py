a3e1baffcfc8ea523c07324c62e37bcfd4507985
N-a290 <nietoangel2902@gmail.com>
2022-08-04 15:20:44 -0500

    Printing the links to the news

--- scraper.py
+++ scraper.py
@@ -1,14 +1,26 @@
 import requests
 import lxml.html as html
 
-XPATH_LINK_TO_ARTICLE = '//div[@class="html-content"]/p/descendant-or-self::text()'
+HOME= 'https://www.larepublica.co'
+
+XPATH_LINK_TO_ARTICLE = '//text-fill[not(@class)]/a/@href'
 XPATH_TITLE= '//div[@class="html-content"]/p/descendant-or-self::text()'
 XPATH_DATE= '//span[@class="date"]/text()'
 XPATH_LEAD= '//div[@class="lead"]/p/text()'
 XPATH_BODY= '//div[@class="html-content"]/p/descendant-or-self::text()'
 
 def parse_home():
-    pass
+    try:
+        response= requests.get(HOME)
+        if response.status_code == 200:
+            home= response.content.decode('utf-8')
+            parsed= html.fromstring(home)
+            links_article= parsed.xpath(XPATH_LINK_TO_ARTICLE)
+            print(links_article)
+        else:
+            raise ValueError(f"Error: {response.status_code}")
+    except ValueError as ve:
+        print(ve)
 
 def run():
     parse_home()
